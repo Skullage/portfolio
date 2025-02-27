37be7692fd4455530fe2e697e11e938373a287d2
evgenydogonadze <dogonadze1999@gmail.com>
2025-02-27 16:14:23 +0500

    Update

--- .nuxt/imports.d.ts
+++ .nuxt/imports.d.ts
@@ -30,5 +30,4 @@ export { withCtx, withDirectives, withKeys, withMemo, withModifiers, withScopeId
 export { injectHead, useHead, useSeoMeta, useHeadSafe, useServerHead, useServerSeoMeta, useServerHeadSafe } from '@unhead/vue';
 export { usePinia } from '../node_modules/@pinia/nuxt/dist/runtime/composables';
 export { useImage } from '../node_modules/@nuxt/image/dist/runtime/composables';
-export { useNuxtDevTools } from '../node_modules/nuxt/node_modules/@nuxt/devtools/dist/runtime/use-nuxt-devtools';
 export { definePageMeta } from '../node_modules/nuxt/dist/pages/runtime/composables';
